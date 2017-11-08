ea9df2d06b0d34a47b52b7c88faf3d76dda2ac6c
Anwita Singh <Anwita Singh>
2017-11-08 18:05:32 -0500

    updated search element

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -4,6 +4,9 @@ class ListContacts extends Component {
   state = {
     query : ''
   }
+  updateQuery = ((query) => {
+    this.setState({query: query.trim()})
+  })
   
   render() {
     //console.log('props', this.props)
@@ -12,9 +15,17 @@ class ListContacts extends Component {
       
       <div className = 'list-contacts'>
 
-        <div className = 'list-contacts-top'>
-          <input type="text"
-          placeholder = 'Search the contacts'/>
+        <div >
+          <input classname = 'search-contacts'
+          type="text"
+          placeholder = 'Search the contacts'
+          //we want the value of the state to be the query element
+          value = {this.state.query}
+          // if the query element changes we want to update the state of the component, onChange event handler will
+          //invoke updateQuery method, which will update the state of the component and after that value will be update
+          //with the current state
+          onChange = {(event) => this.updateQuery(event.target.value)}
+          />
 
         </div>
           <ol className = 'contact-list'> 
